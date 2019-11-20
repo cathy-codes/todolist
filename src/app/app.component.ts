4834f526decdeba280ebf73513138e260683e37d
Cathy <cathy@webigence.com>
2019-11-20 12:32:16 +0000

    Checklist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template:
+  `
+    <h1 class="app-title">
+      Welcome to {{ title }}!
+    </h1>
+    <app-list-manager></app-list-manager>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'My To Do List APP';
+  userLoggedIn = false;
+  
+
 }
